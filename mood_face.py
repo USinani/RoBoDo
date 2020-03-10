67cc6f122cff0c0b2c8b5aaefdfbddbaa1655732
Uljan Sinani <uljan89@gmail.com>
2020-03-10 21:11:41 +0000

    Add files via upload

---
+++ mood_face.py
@@ -0,0 +1,14 @@
+'''
+An attempt to write a program that can portray a cartoon face able to express emotions based on time of day
+
+example: Sleepy during night, wide-eye open during the day, and half-eye open during evening
+
+'''
+
+
+# Body parts: Head, eyes, eyebrows, mouth, ears
+
+
+
+
+# mood: sleepy, awake, tired
